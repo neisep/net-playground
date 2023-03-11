a653810c659e3ac22dd8ad221514cae55e26045b
jimmie <jimmie@neisep.com>
2023-03-11 17:01:47 +0100

    added(project): added a test project
    Wanted too test out a theory i had in my had.

---
+++ PriorityList/PriorityTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PriorityList.Domain;
+using PriorityList.Extensions;
+using System.Collections.Generic;
+
+namespace PriorityList
+{
+    [TestClass]
+    public class PriorityTest
+    {
+        private List<Supplier> _list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _list = new List<Supplier>();
+
+            var supplier1 = CreateSupplier(1, "1001", "1001");
+            var supplier2 = CreateSupplier(2, "1001", "1002");
+
+            _list.Add(supplier1);
+            _list.Add(supplier2);
+        }
+
+        /// <summary>
+        /// This Test was written to try out a theory i had in my head for sometime.
+        /// The problem is i have got multiple suppliers when i search for example 1001 i get two records but one of them are unique but depends on different properties
+        /// Look here
+        /// Supplier1 got number 1 and Account1 is 1001 and Account2 is 1001
+        /// Supplier2 got number 1 and Account1 is 1001 and Account2 is 1002
+        /// Now you see we have a unique records thanks too the properties.
+        /// </summary>
+        [TestMethod]
+        public void TestPriority_Return1002()
+        {
+            var supplier = _list.GetSupplier("1000", "1001", "1002");
+
+            Assert.AreEqual("1002", supplier.Account2);
+        }
+
+        [TestMethod]
+        public void TestPriority_Return1001()
+        {
+            var supplier = _list.GetSupplier("1000", "1001", "1001");
+
+            Assert.AreEqual("1001", supplier.Account2);
+        }
+
+        private Supplier CreateSupplier(int globalIdentification, string account1, string account2)
+        {
+            return new Supplier
+            {
+                Number = "1000",
+                GlobalIdentification = globalIdentification,
+                Account1 = account1,
+                Account2 = account2
+            };
+        }
+    }
+}
---
+++ PriorityList/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PriorityList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PriorityList")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88c62c63-96cd-41c1-829f-dc36dd5bec48")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
