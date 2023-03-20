1c86531117bd01c5676a3d1cda2b60bb3884615a
Jimmie Jönsson <jimmie.jonsson@hogia.se>
2023-03-20 12:50:38 +0100

    fix(project): moved files and moved to new project too collect everything in one project.

---
+++ Datastracture/Datastracture/BinarySearchTree.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Datastracture
+{
+    [TestClass]
+    public class BinarySearchTree
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
