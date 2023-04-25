b4839b6f59a85c0ea1e8db483758e1d2f36acb80
neisep <neisep@neisep.com>
2023-04-25 10:20:54 +0200

    changed(sorting): tried to add a new test for quickSort and learn how to sort abit.

---
+++ Datastracture/Datastracture/Datastracture/Sorting.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Datastracture.Datastracture
+{
+    public class Sorting
+    {
+        //private int[] _unsortedArray = new int[] { 1, 4, 6, 7, 1, 6, 4, 9, 2, 5 };
+
+        //private int[] _sortedArray = new int[10];
+        private Random _random = new Random();
+
+        public int[] QuickSort(int[] unsortedArray, int left, int right)
+        {
+            if(left < right)
+            {
+                //Select Pivot Value
+                var pivotIndex = _random.Next(left, right);
+                int NewPivotValue = Partition(unsortedArray, left, right, pivotIndex);
+
+                QuickSort(unsortedArray, left, NewPivotValue - 1);
+                QuickSort(unsortedArray, NewPivotValue + 1, right);
+            }
+
+            return unsortedArray;
+        }
+
+        private int Partition(int[] unsortedArray, int left, int right, int pivotIndex)
+        {
+            int pivotValue = unsortedArray[pivotIndex];
+
+            Swap(unsortedArray, pivotIndex, right);
+
+            int storeIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (unsortedArray[i] < pivotValue)
+                {
+                    Swap(unsortedArray, i, storeIndex);
+                    storeIndex += 1;
+                }
+            }
+
+            Swap(unsortedArray, storeIndex, right);
+            return storeIndex;
+        }
+
+        private void Swap(int[] data, int left, int right)
+        {
+            var temp = data[left];
+            data[left] = data[right];
+            data[right] = temp;
+        }
+
+        //public void DummySort(int[] unsortedArray, int[] unsortedPartionOne, int[] unsortedPartionTwo)
+        //{
+        //    int counterOne = 0;
+        //    int counterTwo = 0;
+
+        //    var random = new Random().Next(1,10);
+        //    var pivotValue = unsortedArray[random];
+        //    foreach (var i in unsortedArray)
+        //    {
+        //        if (pivotValue >= i)
+        //        {
+        //            unsortedPartionOne[counterOne] = i;
+        //            counterOne++;
+        //        }
+        //        else
+        //        {
+        //            unsortedPartionTwo[counterTwo] = i;
+        //            counterTwo++;
+        //        }
+        //    }
+
+        //    DummySort(unsortedPartionOne, new int[10], new int[10]);
+        //}
+    }
+}
---
+++ Datastracture/Datastracture/DatastractureTestCase.cs
@@ -0,0 +1,22 @@
+﻿using Datastracture.Datastracture;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Versioning;
+
+namespace Datastracture
+{
+    [TestClass]
+    public class DatastractureTestCase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dummy = new Sorting();
+            var unsortedArray = new int[] { 1, 4, 6, 7, 1, 6, 4, 9, 2, 5 }
+            ;
+            var dummy2 = dummy.QuickSort(unsortedArray, 0, unsortedArray.Length-1);
+        }
+    }
+}
