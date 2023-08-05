b2cabe96192b510232954bb4b45437d650ae14d5
jimmie <jimmie@neisep.com>
2023-08-05 13:43:26 +0200

    fix(misc): added install script for nextcloud
    created a install script for nextcloud on ubuntu/debian

---
+++ Labs/InstallScript/nextcloud.sh
@@ -0,0 +1,15 @@
+#sudo apt update && sudo apt upgrade
+#sudo apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql \
+#php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip
+
+#sudo mysql
+
+read my_var
+
+echo $my_var
+
+
+#CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY 'password';
+#CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+#GRANT ALL PRIVILEGES ON nextcloud.* TO 'username'@'localhost';
+#FLUSH PRIVILEGES;
