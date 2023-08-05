a3bb2886b30d31576d6536e17de6d14997fa4cc7
jimmie <jimmie@neisep.com>
2023-08-05 15:09:16 +0200

    fix(misc): update installation script

--- Labs/InstallScript/nextcloud.sh
+++ Labs/InstallScript/nextcloud.sh
@@ -6,47 +6,45 @@
 #I just made this to make it easier for me to install nextcloud next time i need too set it up.
 ##########################################################################################################
 
-#sudo apt update && sudo apt upgrade
-#sudo apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql \
-#php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip
+sudo apt update && sudo apt upgrade
+sudo apt install apache2 mariadb-server libapache2-mod-php php-gd php-mysql \
+php-curl php-mbstring php-intl php-gmp php-bcmath php-xml php-imagick php-zip
 
-#sudo mysql
+sudo mysql
 
-echo "Set sub domain to use"
+echo "Set a sub domain to use"
 read sub_domain
-#echo "Set password for user: nextcloud on mariadb-server"
-#read my_password
+echo "Set password for user: nextcloud on mariadb-server"
+read my_password
 
 
-#CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY $my_password;
-#CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-#GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';
-#FLUSH PRIVILEGES;
+CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY $my_password;
+CREATE DATABASE IF NOT EXISTS nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';
+FLUSH PRIVILEGES;
 
-#quit;
+quit;
 
-#wget https://download.nextcloud.com/server/releases/latest.zip
-#unzip latest.zip
+wget https://download.nextcloud.com/server/releases/latest.zip
+unzip latest.zip
 
-#sudo cp -r nextcloud /var/www
-#sudo chown -R www-data:www-data /var/www/nextcloud
+sudo cp -r nextcloud /var/www
+sudo chown -R www-data:www-data /var/www/nextcloud
 
-#wget https://raw.githubusercontent.com/neisep/Labs/master/Labs/InstallScript/nextcloud.conf
-#sudo cp -r nextcloud.conf /etc/apache2/sites-available/nextcloud.conf
+wget https://raw.githubusercontent.com/neisep/Labs/master/Labs/InstallScript/nextcloud.conf
 
-#a2ensite nextcloud.conf
+sed -i ''s/ServerName .*/ServerName ${sub_domain}/'' nextcloud.conf
+sudo cp -r nextcloud.conf /etc/apache2/sites-available/nextcloud.conf
+
+a2ensite nextcloud.conf
 
 #required modules
-#a2enmod rewrite
+a2enmod rewrite
 
 #recommended modules
-#a2enmod headers
-#a2enmod env
-#a2enmod dir
-#a2enmod mime
+a2enmod headers
+a2enmod env
+a2enmod dir
+a2enmod mime
 
-#sudo service apache2 restart
-
-
-sed_param=s/Var1=.*/Var1=${sub_domain}/  
-sed -i "$sed_param" file.txt
+sudo service apache2 restart
